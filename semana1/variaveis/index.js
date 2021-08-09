805ba5ff26c5de4507367c47f43fa47b29aa4ab1
marina ramminger <ramminger.marina@gmail.com>
2021-08-09 17:28:04 -0300

    realizei os exercícios de interpretação de código

---
+++ semana1/variaveis/index.js
@@ -0,0 +1,32 @@
+console.log("teste teste teste")
+console.log("mais uma vez")
+console.log("mais uma tentativa")
+
+/*
+let a = 10
+let b = 10
+console.log(b)
+b = 5
+console.log(a,b)
+
+Let é uma varíaval que pode sempre ter o valor alterado. Assim, ela irá imprimir no consolo 10 e abaixo 10 5, 
+já que será considerado o último valor atribuído à variável
+
+
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+console.log(a,b,c)
+
+Ele irá imrprmir no console 10 10 10, já que let pode ter o valor alterado e, 
+dessa forma, a variável irá assumir o último valor atribuído.
+
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+
+Foram criadas strings "p" e "t", sendo ambas let e, assim, podendo assumir diferentes valores. Com o alert,
+quando respondemos essas perguntas, será aberta uma caixinha que irá calcular a razão de t/p, ou seja, irá demonstrar
+quanto eu recebo por hora.
